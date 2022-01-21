7f23679c869eb4dd91cd43363b396e990938b50a
binkng <94534751+binkng@users.noreply.github.com>
2022-01-21 23:25:08 +0800

    Add files via upload

---
+++ test.1.21/test.1.21/关于malloc和celloc.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int* p = (int*)malloc(sizeof(int) * 10);
+//	if (p == NULL)
+//		return 1;
+//	int i = 0;
+//	for (i = 0; i <10; i++)
+//	{
+//		printf("%d \n", *(p + i));
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
---
+++ test.1.21/test.1.21/指针和数组间的转换.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	int A[5] = { 1,2,3,4,5 };
+	int len = 0;
+	printf("请输入你所需要分配的数组的长度： len = \n");
+	scanf("%d", &len);
+	int* pArr = (int*)malloc(sizeof(int) * len);
+
+	for (int i = 0; i < len; ++i)
+	{
+		//scanf("%d",&pArr[i]);
+		scanf("%d", pArr + i);
+	}
+	for (int i = 0; i < len; ++i)
+	{
+		printf("%d\n", pArr[i]);
+	}
+	for (int i = 0; i < len; ++i)
+	{
+		printf("这是A数组的值:\n");
+		printf("%d\n", A[i]);
+		//printf("%d\n",*(pArr + i));
+	}
+	return 0;
+}
