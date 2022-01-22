9fcbcfd96c6260f4c4188c1a5b16d1d2980e64f7
binkng <94534751+binkng@users.noreply.github.com>
2022-01-22 23:28:35 +0800

    Add files via upload

---
+++ test.1.22/test.1.22/test.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 3;
+//	scanf("%d", &i);
+//	printf("i=%d", i);
+//	return 0;
+//}
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char c = getche();
+	return 0;
+}
