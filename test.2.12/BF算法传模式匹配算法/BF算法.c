4b7726a232ec81c3892fd2c101a7aa732cd040b8
binkng <94534751+binkng@users.noreply.github.com>
2022-02-12 22:38:57 +0800

    Add files via upload

---
+++ test.2.12/BF算法传模式匹配算法/BF算法.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void Index_BF(char* A,char* B)
+{
+	char* a = A;
+	char* b = B;//  定义两个操作指针
+	
+}
+int main()
+{
+	char a[] = "abs";
+	char b[20] = "adjgnababs";
+	Index_BF(a, b);
+	return 0;
+}
