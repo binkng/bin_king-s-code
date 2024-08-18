4ceb5be67f303310da3a71f322fa6f50efc8e074
binkng <94534751+binkng@users.noreply.github.com>
2024-08-18 23:31:48 +0800

    Add files via upload

---
+++ 数据存储.c
@@ -0,0 +1,9 @@
+//#include<stdio.h>
+//int main() 
+//{
+//	char a = 128;
+//	printf("%d\n", a);// -128
+//	char b = -129;
+//	printf("%d\n", b);// 127
+//	return 0;
+//}
---
+++ 进制.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;// 十进制
+//	int b = 0100;// 八进制
+//	int c = 0x10;//十六进制
+//	int d = 0b10;//二进制
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	printf("%d\n", c);
+//	printf("%d\n", d);
+//	return 0;
+//}
